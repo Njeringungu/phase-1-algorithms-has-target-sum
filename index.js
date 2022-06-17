f21da5bb14e8f32af95213e64daf761da495d4dd
Janet Njeri Ngungu <janetnjerin89@gmail.com>
2022-06-17 07:45:15 +0300

    adding algorithm has target sum

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
-}
-
+   const availableNumbers = {};
+  
+    for (const number of array) {
+      
+      const num = target - number;
+      if (num in availableNumbers) return true;
+      availableNumbers[number] = true;
+    }
+  
+    return false;
+  }
 /* 
   Write the Big O time complexity of your function here
 */
-
+//Linear O(n)
 /* 
   Add your pseudocode here
 */
-
+//iterate through every element in the array.
+//if the number is in the array return true
 /*
   Add written explanation of your solution here
 */
+//If we do for (const number of array), we iterates over all the values of the array, with each iteration having a value stored in the array.
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
